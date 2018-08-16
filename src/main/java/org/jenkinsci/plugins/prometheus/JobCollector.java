f0cb947ea7b465dd63291beda2aaed4e228cc05f
Chen Fliesher <cf9536@att.com>
2018-08-16 15:54:46 +0300

    Remove redundent ;

--- src/main/java/org/jenkinsci/plugins/prometheus/JobCollector.java
+++ src/main/java/org/jenkinsci/plugins/prometheus/JobCollector.java
@@ -196,7 +196,7 @@ public class JobCollector extends Collector {
 
     protected void appendJobMetrics(Job job, Boolean ignoreBuildMetrics) {
         // Add this to the repo as well so I can group by Github Repository 
-        String[] labelValueArray = {job.getFullName(),StringUtils.substringBetween(job.getFullName(), "/");};
+        String[] labelValueArray = {job.getFullName(),StringUtils.substringBetween(job.getFullName(), "/")};
 
         Run run = job.getLastBuild();
         // Never built
