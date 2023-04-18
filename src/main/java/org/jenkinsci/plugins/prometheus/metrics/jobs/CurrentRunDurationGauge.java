1c8c1fc12db28dccfe7d277fa581e7413cb0689a
waschndolos <killear@gmx.de>
2023-04-18 19:00:39 +0200

    Removing running label. Only calculating metric if there is a running build

--- src/main/java/org/jenkinsci/plugins/prometheus/metrics/jobs/CurrentRunDurationGauge.java
+++ src/main/java/org/jenkinsci/plugins/prometheus/metrics/jobs/CurrentRunDurationGauge.java
@@ -19,28 +19,21 @@ public class CurrentRunDurationGauge extends BaseJobMetricCollector<Job, Gauge> 
                 .name(calculateName("running_build_duration_milliseconds"))
                 .subsystem(subsystem)
                 .namespace(namespace)
-                .labelNames(ArrayUtils.appendToArray(labelNames, "building"))
+                .labelNames(labelNames)
                 .help("Indicates the runtime of the run currently building if there is a run currently building")
                 .create();
     }
 
     @Override
     public void calculateMetric(Job jenkinsObject, String[] labelValues) {
-        boolean isBuilding = jenkinsObject.isBuilding();
 
-        String[] labels = ArrayUtils.appendToArray(labelValues, String.valueOf(isBuilding));
-        if (isBuilding) {
-            Run runningBuild = jenkinsObject.getLastBuild();
-            if (runningBuild != null) {
-                long start = runningBuild.getStartTimeInMillis();
-                // Using Clock to be able to mock in test
-                long end = Clock.systemUTC().millis();
-                long duration = Math.max(end - start, 0);
-                this.collector.labels(labels).set(duration);
-                return;
-            }
+        Run runningBuild = jenkinsObject.getLastBuild();
+        if (runningBuild != null && runningBuild.isBuilding()) {
+            long start = runningBuild.getStartTimeInMillis();
+            // Using Clock to be able to mock in test
+            long end = Clock.systemUTC().millis();
+            long duration = Math.max(end - start, 0);
+            this.collector.labels(labelValues).set(duration);
         }
-
-        this.collector.labels(labels).set(0.0);
     }
 }
