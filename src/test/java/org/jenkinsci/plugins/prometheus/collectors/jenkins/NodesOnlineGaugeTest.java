15e608424244b8bd6fa5e596cd9b94d8ca3eb3f1
Aleksei Ioffe <aleksei@scanbot.io>
2023-10-26 11:58:01 +0200

    Fix broken tests

--- src/test/java/org/jenkinsci/plugins/prometheus/collectors/jenkins/NodesOnlineGaugeTest.java
+++ src/test/java/org/jenkinsci/plugins/prometheus/collectors/jenkins/NodesOnlineGaugeTest.java
@@ -70,9 +70,6 @@ public class NodesOnlineGaugeTest extends MockedJenkinsTest {
 
     static void setFinalStatic(Field field, Object newValue) throws Exception {
         field.setAccessible(true);
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
         field.set(null, newValue);
     }
 }
